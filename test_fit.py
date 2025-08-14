1ee4055b846face1c33007a481e07180d3183cd9
barentine <31105780+barentine@users.noreply.github.com>
2025-08-14 14:59:02 -0700

    get the test in order (filter sigma was not set correctly)

--- test_fit.py
+++ test_fit.py
@@ -8,47 +8,43 @@ fit_mod = 'double_helix.DoubleGaussFit'
 md['StartTime'] = 1300676151.901
 md['tIndex'] = 0
 md['Analysis.BGRange'] = [0, 0]
-# md['Analysis.DataFileID'] = 1571469165
-# md['Analysis.DebounceRadius'] = 14
-md['Analysis.DetectionThreshold'] = 1# 0.75E6
+
+md['Analysis.DetectionThreshold'] = 1
 md['Analysis.FitModule'] = fit_mod
-# md['Analysis.InterpModule'] = 'CSInterpolator'
-#md['Analysis.EstimatorModule'] = 'priEstimator'
+
 md['Analysis.subtractBackground'] = False
 md['Analysis.ROISize'] = 10
-md['Camera.ADOffset'] = 110
+md['Camera.ADOffset'] = 178
 md['Camera.CycleTime'] = 0.25178998708724976
-md['Camera.EMGain'] = 272
+md['Camera.EMGain'] = 300
 md['Camera.ElectronsPerCount'] = 12.9
 md['Camera.IntegrationTime'] = 0.25
 md['Camera.Name'] = 'Andor IXon DV97'
 md['Camera.NoiseFactor'] = 1.4
 md['Camera.ReadNoise'] = 1
-md['Camera.TrueEMGain'] = 33.415239495686144
+md['Camera.TrueEMGain'] = 284
 md['voxelsize.units'] = 'um'
-md['voxelsize.x'] = 0.1177
-md['voxelsize.y'] = 0.1177
+md['voxelsize.x'] = 0.12
+md['voxelsize.y'] = 0.12
 md['voxelsize.z'] = 0.1
 md['Analysis.SigmaGuess'] = 200  # Double Helix Lobe Sigma Guess [nm]
-md['Analysis.LobeSepGuess'] = 900  # Double Helix Lobe Separation Guess [nm]
-md['Analysis.DetectionFilterSigma'] = 8  # Detection Filter Sigma (in px)
+md['Analysis.LobeSepGuess'] = 1050  # Double Helix Lobe Separation Guess [nm]
+md['Analysis.DetectionFilterSigma'] = 5  # Detection Filter Sigma (in px)
 
 # A0, A1, x, y, theta, lobe_sep, sig, bg = p
-md['Test.DefaultParams'] = [2000, 2000, 0, 0, 0, 900, 200, 50]
-md['Test.ParamJitter'] = [200, 200, 117, 117, np.pi, 100, 25, 25]
-# md['Test.SimModule'] = u'InterpFitR'
+md['Test.DefaultParams'] = [500, 500, 0, 0, 0, md['Analysis.LobeSepGuess'], md['Analysis.SigmaGuess'], 20]
+md['Test.ParamJitter'] = [50, 50, 120, 120, 0.5*np.pi, 20, 10, 10]
 md['Test.ROISize'] = md['Analysis.ROISize']
 
 
 def test_DoubleHelixFit_Theta():
-    """Test the Astigmatic Gaussian fit by fitting some randomly generated events. The pass condition here is fairly
-    loose, but should be sufficient to detect when the code has been broken"""
+    """flexible, but hopefully sufficient to detect when the code has been broken"""
     from PYME.localization.Test import fitTestJigWC as fitTestJig
 
     tj = fitTestJig.fitTestJig(md, fit_mod)
     tj.runTests(nTests=100)
-
-    errors_over_pred_IQR = fitTestJig.IQR((tj.error('x0') / tj.res['fitError']['x0']))
-    print(errors_over_pred_IQR)
     
-    assert errors_over_pred_IQR < 3
+    assert np.abs(tj.error('x0')).mean() < 0.1 * (md['voxelsize.x'] * 1e3)  # 0.1 [pix] in [nm]
+    assert np.abs(tj.error('theta')).mean() < 0.1  # [rad.]
+
+test_DoubleHelixFit_Theta()
